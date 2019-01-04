8a91ad91aa0cb182d2e54999dfe85266c3a028fa
Richard Bullock <richardbullock86@gmail.com>
2019-01-04 15:03:43 -0700

    main layout

--- CodePop/Controllers/HomeController.cs
+++ CodePop/Controllers/HomeController.cs
@@ -12,26 +12,7 @@ namespace CodePop.Controllers
     {
         public IActionResult Index()
         {
-            return View();
-        }
-
-        public IActionResult About()
-        {
-            ViewData["Message"] = "Your application description page.";
-
-            return View();
-        }
-
-        public IActionResult Contact()
-        {
-            ViewData["Message"] = "Your contact page.";
-
-            return View();
-        }
-
-        public IActionResult Privacy()
-        {
-            return View();
+            return View("Index");
         }
 
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
