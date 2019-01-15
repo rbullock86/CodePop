c38f2c3afdb325eace296d74355832a5be866968
Richard Bullock <richardbullock86@gmail.com>
2019-01-15 16:45:10 -0700

    Topbar and sidebar baseline functionality

--- CodePop/wwwroot/js/site.js
+++ CodePop/wwwroot/js/site.js
@@ -2,3 +2,9 @@
 // for details on configuring this project to bundle and minify static web assets.
 
 // Write your JavaScript code.
+
+$(document).ready(function () {
+/*Topbar menu dropdown functionality*/
+    $("#topbar-dropdown").click(function () { $("#sidebar").toggleClass("xs-hide") });
+    $(".main-body").hover(function() { $("#sidebar").addClass("xs-hide") });
+});
